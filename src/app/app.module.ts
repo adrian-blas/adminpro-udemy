af8bcf3a193c02015285aa33a37019b8788e9ee9
militho <militho.blazitho@gmail.com>
2018-05-04 18:15:26 -0500

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { APP_ROUTES } from './app.routes';
 // Modules
 import { PagesModule } from './pages/pages.module';
 
+// temporal
+import { FormsModule } from '@angular/forms';
+
 // Components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -22,7 +25,9 @@ import { RegisterComponent } from './login/register.component';
   imports: [
     BrowserModule,
     APP_ROUTES,
-    PagesModule
+    PagesModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,17 +4,22 @@ import { PAGES_ROUTES } from './pages.routes';
 
 import { SharedModule } from '../shared/shared.module';
 
+import { FormsModule } from '@angular/forms';
+
+
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
 
 @NgModule({
   declarations:[
     PagesComponent,
     DashboardComponent,
     ProgressComponent,
-    Graficas1Component
+    Graficas1Component,
+    IncrementadorComponent
   ],
   exports:[
     DashboardComponent,
@@ -23,7 +28,8 @@ import { Graficas1Component } from './graficas1/graficas1.component';
   ],
   imports:[
     SharedModule,
-    PAGES_ROUTES
+    PAGES_ROUTES,
+    FormsModule
   ]
 })
 
