5fb180906ae27121ce1999de8d65434afc425810
militho <militho.blazitho@gmail.com>
2018-05-14 17:45:49 -0500

    Fin de la seccion 7

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,7 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RouterModule } from '@angular/router';
 
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { HeaderComponent } from './header/header.component';
@@ -19,7 +22,8 @@ import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
     BreadcrumbsComponent,
   ],
   imports:[
-
+    RouterModule,
+    CommonModule
   ]
 })
 
